d870f328895d03df9de06302011bb96dbdd7d502
kenta-afk <kentarou9.60@icloud.com>
2024-11-27 08:49:03 +0900

    id指定

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ var albums = []album{
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
 	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
@@ -34,6 +35,20 @@ func main() {
 
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
+}
+
+
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album Not Found"})
 }
 
 
